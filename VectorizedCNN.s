fd69ab070d8a838b23c9887f2303e2c6b60930a3
Aman <159479526+AmanTheDrummer@users.noreply.github.com>
2025-06-04 20:13:58 +0500

    Update VectorizedCNN.s

--- VectorizedCNN.s
+++ VectorizedCNN.s
@@ -1,11 +1,18 @@
+#define STDOUT 0xd0580000
+
+.section .text
+.global _start
+_start:
+## START YOUR CODE HERE
+
 # CNN Forward Pass in RISC-V Assembly using Vector Instructions (Fixed and Vectorized)
 
-.include "conv_filters.asm"
-.include "W1.asm"
-.include "B1.asm"
-.include "W2.asm"
-.include "B2.asm"
-.include "InputVector.asm"
+.include "./assembly/conv_filters.asm"
+.include "./assembly/W1.asm"
+.include "./assembly/B1.asm"
+.include "./assembly/W2.asm"
+.include "./assembly/B2.asm"
+.include "./assembly/InputVector.asm"
 
 .data
 ConvOut: .space 1152     # 8 filters * 6x6 output * 4 bytes
@@ -36,8 +43,12 @@ conv_x_loop:
     la x9, patch_buffer     # Temp patch store (assumed aligned)
 patch_gather_loop:
     li x10, 8
-    rem x11, x8, 3          # dx
-    div x12, x8, 3          # dy
+    li x3,3
+    div x12,x8,x3
+    mul x13,x12,x3 
+    sub x11,x8,x13
+    li x3,3 
+    div x12, x8, x3          # dy
     add x13, x12, x6        # iy = dy + y
     add x14, x11, x7        # ix = dx + x
     li x15, 8               # image width
@@ -53,11 +64,12 @@ patch_gather_loop:
     li x20, 9
     blt x8, x20, patch_gather_loop
 
-    vlw.v v1, (x9)                  # input patch
-    mul x21, x5, 36
+    vle32.v v1, (x9)                  # input patch
+    li x10, 36
+    mul x21, x5, x10 
     slli x21, x21, 2
     add x22, x3, x21
-    vlw.v v2, (x22)                # filter weights
+    vle32.v v2, (x22)                # filter weights
 
     vfmul.vv v3, v1, v2
     vmv.v.x v0, x0
@@ -65,8 +77,11 @@ patch_gather_loop:
     vmv.x.s x23, v4
     fmv.w.x f10, x23
 
-    mul x24, x5, 36
-    mul x25, x6, 6
+    li x11, 36
+    mul x24, x5, x11 
+    li x12, 6
+    mul x25, x6, x12
+
     add x25, x25, x7
     add x24, x24, x25
     slli x24, x24, 2
@@ -108,8 +123,8 @@ relu_loop:
 
     li x6, 0       # neuron index
 fc1_loop:
-    vlw.v v1, (x2)
-    vlw.v v2, (x3)
+    vle32.v v1, (x2)
+    vle32.v v2, (x3)
     vfmul.vv v3, v1, v2
     vmv.v.x v0, x0
     vredsum.vs v4, v3, v0
@@ -135,8 +150,8 @@ fc1_loop:
     la x5, Z2
     li x6, 0
 fc2_loop:
-    vlw.v v1, (x2)
-    vlw.v v2, (x3)
+    vle32.v v1, (x2)
+    vle32.v v2, (x3)
     vfmul.vv v3, v1, v2
     vmv.v.x v0, x0
     vredsum.vs v4, v3, v0
@@ -181,3 +196,68 @@ end:
 .data
 .align 4
 patch_buffer: .space 36       # 9 floats for 3x3 input patch
+## END YOU CODE HERE
+
+# Function: print
+# Logs values from array in a0 into registers v1 for debugging and output.
+# Inputs:
+#   - a0: Base address of array
+#   - a1: Size of array i.e. number of elements to log
+# Clobbers: t0,t1, t2,t3 ft0, ft1.
+printToLogVectorized:        
+    addi sp, sp, -4
+    sw a0, 0(sp)
+
+    li t0, 0x123                 # Pattern for help in python script
+    li t0, 0x456                 # Pattern for help in python script
+    mv a1, a1                   # moving size to get it from log 
+    mul a1, a1, a1              # sqaure matrix has n^2 elements 
+	li t0, 0		                # load i = 0
+    printloop:
+        vsetvli t3, a1, e32           # Set VLEN based on a1
+        slli t4, t3, 2                # Compute VLEN * 4 for address increment
+
+        vle32.v v1, (a0)              # Load real[i] into v1
+        add a0, a0, t4                # Increment pointer for real[] by VLEN * 4
+        add t0, t0, t3                # Increment index
+
+        bge t0, a1, endPrintLoop      # Exit loop if i >= size
+        j printloop                   # Jump to start of loop
+    endPrintLoop:
+    li t0, 0x123                    # Pattern for help in python script
+    li t0, 0x456                    # Pattern for help in python script
+	
+    lw a0, 0(sp)
+    addi sp, sp, 4
+
+	jr ra
+
+
+
+# Function: _finish
+# VeeR Related function which writes to to_host which stops the simulator
+_finish:
+    li x3, 0xd0580000
+    addi x5, x0, 0xff
+    sb x5, 0(x3)
+    beq x0, x0, _finish
+
+    .rept 100
+        nop
+    .endr
+
+
+.data
+## ALL DATA IS DEFINED HERE LIKE MATRIX, CONSTANTS ETC
+
+
+## DATA DEFINE START
+.equ MatrixSize, 5
+matrix:
+    .float -10.0, 13.0, 10.0, -3.0, 2.0
+    .float 6.0, 15.0, 4.0, 13.0, 4.0
+    .float 18.0, 2.0, 9.0, 8.0, -4.0
+    .float 5.0, 4.0, 12.0, 17.0, 6.0
+    .float -10.0, 7.0, 13.0, -3.0, 160.0
+## DATA DEFINE END
+size: .word MatrixSize
